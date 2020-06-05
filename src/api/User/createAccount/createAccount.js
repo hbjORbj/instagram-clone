311f1b54ebe950542923229f8572f23abcb6b0d4
hbjORbj <sldisek783@gmail.com>
2020-06-05 17:27:39 +0100

    Update createAccount

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,9 +3,17 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     createAccount: async (_, args) => {
-      const { username, email } = args;
-      const user = await prisma.createUser({ username, email });
-      return user;
+      const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const usernameExists = await prisma.$exists.user({ username });
+      const emailExists = await prisma.$exists.user({ email });
+      if (usernameExists) {
+        throw Error("This username already exists.");
+      } else if (emailExists) {
+        throw Error("This email has already been used to create an account.");
+      } else {
+        await prisma.createUser({ username, email, firstName, lastName, bio });
+        return true;
+      }
     },
   },
 };
